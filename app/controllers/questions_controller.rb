37b63ae213f4ba89357615205d1a40e6d6c370fc
prostasmosta <prostasmosta@gmail.com>
2022-04-28 22:23:24 +0300

    added partials to views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,25 +4,26 @@ class QuestionsController < ApplicationController
   def create 
     question = Question.create(question_params)
 
-    redirect_to question_path(question)
+    redirect_to question_path(question), notice: 'Новый вопрос создан!'
   end
 
   def update
     @question.update(question_params)
 
-    redirect_to question_path(@question)
+    redirect_to question_path(@question), notice: 'Вопрос сохранен!'
   end
 
   def destroy
     @question.destroy
 
-    redirect_to questions_path
+    redirect_to questions_path, notice: 'Вопрос удален!'
   end
 
   def show
   end
 
   def index
+    @question = Question.new
     @questions = Question.all
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
-  
+
   resources :questions do
     patch :hide, on: :member
   end
+  resources :users, only: %i[new create]
 end
