ab919a293f7c10f3dedaa1ff9426787b7d6ef3e7
prostasmosta <prostasmosta@gmail.com>
2022-04-30 16:08:41 +0300

    created users controller and view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def new
+    session[:current_time] = Time.now
+    @user = User.new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:name, :nickname, :email)
+
+    user = User.create(user_params)
+    session[:user_id] = user.id
+
+    redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+  end
+end
