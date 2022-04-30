847a9a0a9f1f6b054ec1167b040992fcfe842f02
prostasmosta <prostasmosta@gmail.com>
2022-04-30 17:13:53 +0300

    added password and minor styles corrections

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email)
+    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
 
     user = User.create(user_params)
     session[:user_id] = user.id
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_28_182221) do
+ActiveRecord::Schema.define(version: 2022_04_30_135909) do
 
   create_table "questions", force: :cascade do |t|
     t.text "body"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_04_28_182221) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
