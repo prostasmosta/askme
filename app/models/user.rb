cae4141de61ad9e0302a7739ee02ccfbfe7802c7
prostasmosta <prostasmosta@gmail.com>
2022-05-01 16:03:09 +0300

    regexp fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
-  before_save :downcase_nickname
+  EMAIL_REGEXP = /\A[\w+\-.]+@[\w+\-.]+\.[a-z]+\z/
+  NICKNAME_REGEXP = /\A\w+\z/
 
-  EMAIL_REGEXP = /\A[\w+\-.]+@[\w+\-.]+\.[a-z]+\z/i
-  NICKNAME_REGEXP = /\A[\w]+\z/
+  before_save :downcase_nickname
 
   validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEXP }
   validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: NICKNAME_REGEXP }
