8e76e6e46437598378eae905dca3c89001e70faa
prostasmosta <prostasmosta@gmail.com>
2022-05-01 13:56:30 +0300

    new validations for user and question models added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,15 +2,25 @@ class QuestionsController < ApplicationController
   before_action :set_question, only: %i[update show destroy edit hide]
 
   def create 
-    question = Question.create(question_params)
+    @question = Question.new(question_params)
 
-    redirect_to question_path(question), notice: 'Новый вопрос создан!'
+    if @question.save
+      redirect_to question_path(@question), notice: 'Новый вопрос создан!'
+    else
+      flash.now[:alert] = 'Вы неправильно заполнили поля'
+
+      render :new
+    end
   end
 
   def update
-    @question.update(question_params)
+    if @question.update(question_params)
+      redirect_to question_path(@question), notice: 'Вопрос обновлен!'
+    else
+      flash.now[:alert] = 'При попытке сохранить вопрос возникли ошибки'
 
-    redirect_to question_path(@question), notice: 'Вопрос сохранен!'
+      render :edit
+    end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
 
   before_save :downcase_nickname
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[\w+\-.]+\.[a-z]+\z/i}
+  validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: /\A[a-z_1-9]+\z/ }
 
   def downcase_nickname
     nickname.downcase!
