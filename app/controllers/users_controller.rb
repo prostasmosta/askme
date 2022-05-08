999e9bbb3c46e23da8ae8f239a02853f3d42de0c
prostasmosta <prostasmosta@gmail.com>
2022-05-08 11:49:03 +0300

    fixed param id->nickname

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
   end
 
   def set_user
-    @user = User.find_by_nickname(params[:id])
+    @user = User.find_by_nickname(params[:nickname])
   end
 
   def authorize_user
